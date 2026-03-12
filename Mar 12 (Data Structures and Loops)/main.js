7d400699a66da5c108a872c287ad092a1b4970bb
Robin Banks <umair.almahmood10@gmail.com>
2026-03-12 14:56:16 -0400

    March 12

---
+++ Mar 12 (Data Structures and Loops)/main.js
@@ -0,0 +1,69 @@
+// Data Structures and Loops
+
+// Arrays
+// let myArray = [1, 2, 3, 4, 5];
+
+// Accessing elements
+// console.log(myArray[0]); 
+// console.log(myArray[2]); 
+
+// Modifying elements
+// myArray[1] = 10;
+// console.log(myArray); 
+
+
+// Loops
+
+// For loop
+//for (let i = 0; i < myArray.length; i++) {
+//    console.log(myArray[i]);
+//}
+
+// For of loop
+//for (let element of myArray) {
+//    console.log(element);
+//}
+
+// While loop
+//let j = 0;
+//while (j < myArray.length) {
+ //   console.log(myArray[j]);
+//    j++;
+//}
+
+// Do while loop
+//let k = 0;
+//do {
+//    console.log(myArray[k]);
+//    k++;
+//} while (k < myArray.length);
+
+
+// Objects
+//let Person = {
+//    name: "Carl",
+//    age: 50,
+//    city: "New York"
+//};
+
+//console.log(Person.name); 
+//console.log(Person.age); 
+//console.log(Person.city); 
+
+// Classes
+//class PlayingCard {
+//    constructor(suit, rank) {
+//        this.suit = suit;
+//        this.rank = rank;
+//    }
+
+//    toString() {
+//        return `${this.rank} of ${this.suit}`;
+//    }
+//}
+
+//let card1 = new PlayingCard("Spades", "Ace");
+//console.log(card1.toString());
+
+//let card2 = new PlayingCard("Hearts", "Queen");
+//console.log(card2.toString());
